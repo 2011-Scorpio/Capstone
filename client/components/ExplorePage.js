c553e8b7fabffc5e4b039a7ede447a4b983b8ca7
Gabriel Smith <gabesmithp@gmail.com>
2021-02-02 15:45:05 -0500

    add: player and play button

--- client/components/ExplorePage.js
+++ client/components/ExplorePage.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import {Play, FastForward} from 'react-feather'
 
 const ExplorePage = () => {
+  const playSong = () => {}
+
   return (
     <div className="explore-page-container f jcc">
-      <div className="player" />
+      <audio src="" />
+      <div className="player f jcc aie">
+        <div className="f jcb">
+          {/* <li className="player-btn"></li> */}
+          <button
+            className="player-btn f"
+            type="button"
+            onClick={() => playSong()}
+          >
+            <Play />
+          </button>
+          {/* <li className="player-btn"><FastForward/></li> */}
+        </div>
+      </div>
     </div>
   )
 }
