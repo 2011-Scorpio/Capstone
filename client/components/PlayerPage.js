774bf5e943763ed7850d477826f72cc86791914c
Gabriel Smith <gabesmithp@gmail.com>
2021-02-02 21:55:44 -0500

    add: play/pause toggle

--- client/components/PlayerPage.js
+++ client/components/PlayerPage.js
@@ -1,28 +1,40 @@
 import React, {useState, useRef, useEffect} from 'react'
-import {Play, FastForward} from 'react-feather'
+import {Play, FastForward, Pause} from 'react-feather'
 import {connect} from 'react-redux'
 
 const PlayerPage = props => {
+  const [isPlaying, setPlaying] = useState(false)
+
   let currentSong = props.album
     ? props.album.tracks.items[0].preview_url
     : 'nothing here'
-  console.log(currentSong)
-  const [isPlaying, setIsPlaying] = useState(false)
+
+  const togglePlay = () => setPlaying(!isPlaying)
 
   return (
     <div className="explore-page-container f jcc">
       <div className="player f jcc aie">
-        <audio controls autoPlay src={currentSong} />
+        <audio id="player control" src={currentSong} />
         <div className="f jcb">
           {/* <li className="player-btn"></li> */}
           <button
             className="player-btn f"
             type="button"
-            // onClick={() => playSong()}
+            onClick={() => togglePlay()}
           >
-            <Play />
+            {isPlaying ? <Pause /> : <Play />}
           </button>
           {/* <li className="player-btn"><FastForward/></li> */}
+
+          {/* <button onclick="document.getElementById('player').pause()">
+              Pause
+            </button>
+            <button onclick="document.getElementById('player').volume += 0.1">
+              Vol +
+            </button>
+            <button onclick="document.getElementById('player').volume -= 0.1">
+              Vol -
+            </button> */}
         </div>
       </div>
     </div>
