e14ce3afcc8e5782fe2db74cab08d5816ecb6912
Azriel <goldmanazriel@yahoo.com>
2021-02-03 12:19:59 -0500

    Front End: Added Connect mapState and mapDispatch to AllPlaylists Component

--- client/components/AllPlaylists.js
+++ client/components/AllPlaylists.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {fetchAllPlaylists} from '../store/allPlaylists'
 
 export class AllPlaylists extends React.Component {
   render() {
-    return <React.Fragment />
+    return (
+      <React.Fragment>
+        <div>Listing All Playlists:</div>
+      </React.Fragment>
+    )
   }
 }
+
+const mapState = state => ({playlists: state.playlists})
+const mapDispatch = dispatch => ({
+  getPlaylists: () => dispatch(fetchAllPlaylists())
+})
+export default connect(mapState, mapDispatch)(AllPlaylists)
