468a2e8abd940c750b329d8af7fd660e59abc6cb
Azriel <goldmanazriel@yahoo.com>
2021-02-03 15:13:33 -0500

    Front End: Created SinglePlaylist React Component

---
+++ client/components/SinglePlaylist.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {fetchSinglePlaylist} from '../store/singlePlaylist'
+
+export class SinglePlaylist extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div>Single Playlist</div>
+      </React.Fragment>
+    )
+  }
+}
