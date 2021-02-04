751577ff3263355e4628044adc8065541e82a72c
Spencer Collins <smcollins36@gmail.com>
2021-02-04 10:50:48 -0600

    created userplaylist store to create new playlist in user's spotify account

---
+++ client/store/userPlaylist.js
@@ -0,0 +1,39 @@
+import axios from 'axios'
+
+const CREATE_PLAYLIST = 'CREATE_PLAYLIST'
+
+const createPlaylist = playlistId => ({
+  type: CREATE_PLAYLIST,
+  playlistId
+})
+
+export const makePlaylist = (userId, token) => {
+  return async dispatch => {
+    try {
+      const {data} = await axios({
+        url: `https://api.spotify.com/v1/users/${userId}/playlists`,
+        method: 'post',
+        headers: {
+          Authorization: 'Bearer ' + token
+        },
+        params: {
+          name: 'Omakase'
+        }
+      })
+      dispatch(createPlaylist(data))
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+let initialState = {}
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case CREATE_PLAYLIST:
+      return action.playlistId
+    default:
+      return state
+  }
+}
