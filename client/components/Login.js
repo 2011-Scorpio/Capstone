f3c5d7242a73dc419bd08cee2e67c28d70889009
Gabriel Smith <gabesmithp@gmail.com>
2021-02-08 13:25:35 -0500

    add: spotify logo to login page

--- client/components/Login.js
+++ client/components/Login.js
@@ -2,10 +2,15 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div className="login-form f jcc aie">
-      <a href="/auth/spotify" className="button-two">
-        Log In
-      </a>
+    <div className="login-form">
+      <div>
+        <img className="spotify-logo" src="/images/spotify.png" alt="" />
+      </div>
+      <div>
+        <a href="/auth/spotify" className="button-two">
+          Log In
+        </a>
+      </div>
     </div>
   )
 }
