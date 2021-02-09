dd95bf3ef38d1574158da51559ada87ae82ae5a1
Spencer Collins <smcollins36@gmail.com>
2021-02-09 14:15:14 -0600

    created class method to process audi feature data, plus framwork of radar chart

--- client/components/RdrChart.js
+++ client/components/RdrChart.js
@@ -1,10 +1,56 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {fetchUserPlaylist, fetchAudioFeat} from '../store/spotify'
-import {RadarChart} from 'recharts'
+import {
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+  Radar
+} from 'recharts'
 import {me} from '../store'
 
 class RdrChart extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      data: []
+    }
+  }
+
+  processChartData(chartDataArr) {
+    let template = [
+      {
+        attribute: 'Danceable',
+        A: 0,
+        fullMark: 100
+      },
+      {
+        attribute: 'Loud',
+        A: 0,
+        fullMark: 100
+      },
+      {
+        attribute: 'Energetic',
+        A: 0,
+        fullMark: 100
+      }
+    ]
+
+    chartDataArr.map(song => {
+      template[0].A += song.danceability * 100
+      template[1].A += song.loudness * -1
+      template[2].A += song.energy * 100
+    })
+    template.map(attribute => {
+      attribute.A /= chartDataArr.length
+    })
+
+    this.setState({
+      data: template
+    })
+  }
+
   async componentDidMount() {
     await this.props.getUser()
     await this.props.getUserPlaylist(this.props.token)
@@ -15,7 +61,27 @@ class RdrChart extends React.Component {
   }
 
   render() {
-    return <RadarChart />
+    return (
+      <RadarChart
+        cx={300}
+        cy={250}
+        outerRadius={150}
+        width={500}
+        height={500}
+        data={this.state.data}
+      >
+        <PolarGrid />
+        <PolarAngleAxis dataKey="attribute" />
+        <PolarRadiusAxis />
+        <Radar
+          name="Mike"
+          dataKey="A"
+          stroke="#8884d8"
+          fill="#8884d8"
+          fillOpacity={0.6}
+        />
+      </RadarChart>
+    )
   }
 }
 
