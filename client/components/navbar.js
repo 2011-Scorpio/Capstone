10585271fb5d7cf1dc6b9de8e1274f114aeb25c0
Gabriel Smith <gabesmithp@gmail.com>
2021-02-09 10:52:00 -0500

    add: menu slides on link click

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,35 +1,54 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 
-const Navbar = () => (
-  <header>
-    <NavLink to="/" className="no-under">
-      <h3 className="navbar-name">Omakase</h3>
-    </NavLink>
-    <input id="nav-toggle" type="checkbox" className="nav-toggle" />
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/login" className="nav-links">
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/explore" className="nav-links">
-            Playlists
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/about" className="nav-links">
-            About
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-    <label htmlFor="nav-toggle" className="nav-toggle-label">
-      <span />
-    </label>
-  </header>
-)
+const Navbar = () => {
+  const toggleCheckbox = () => {
+    const checkbox = document.getElementById('nav-toggle')
+    checkbox.checked = !checkbox.checked
+  }
+
+  return (
+    <header>
+      <NavLink to="/" className="no-under">
+        <h3 className="navbar-name">Omakase</h3>
+      </NavLink>
+      <input id="nav-toggle" type="checkbox" className="nav-toggle" />
+      <nav>
+        <ul>
+          <li>
+            <NavLink
+              to="/login"
+              className="nav-links"
+              onClick={() => toggleCheckbox()}
+            >
+              Login
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/explore"
+              className="nav-links"
+              onClick={() => toggleCheckbox()}
+            >
+              Playlists
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about"
+              className="nav-links"
+              onClick={() => toggleCheckbox()}
+            >
+              About
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <label htmlFor="nav-toggle" className="nav-toggle-label">
+        <span />
+      </label>
+    </header>
+  )
+}
 
 export default Navbar
