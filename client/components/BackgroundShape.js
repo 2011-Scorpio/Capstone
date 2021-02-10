3a6349585b67bad8ba497ca42d827c8bf00e11cc
Spencer Collins <smcollins36@gmail.com>
2021-02-10 11:46:03 -0600

    changed background shape and color

--- client/components/BackgroundShape.js
+++ client/components/BackgroundShape.js
@@ -13,18 +13,23 @@ class BackgroundShape extends React.Component {
     let renderer = new THREE.WebGLRenderer({alpha: true})
     renderer.setSize(window.innerWidth, window.innerHeight)
     document.body.appendChild(renderer.domElement)
-    let geometry = new THREE.BoxGeometry(1, 1, 1)
-    let material = new THREE.MeshBasicMaterial({color: 0x00ff00})
-    let cube = new THREE.Mesh(geometry, material)
-    scene.add(cube)
+    const geometry = new THREE.TorusGeometry(2, 0.75, 15, 100)
+    let material = new THREE.MeshNormalMaterial()
+    let plane = new THREE.Mesh(geometry, material)
+    scene.add(plane)
     camera.position.z = 5
     const animate = function() {
       requestAnimationFrame(animate)
-      cube.rotation.x += 0.01
-      cube.rotation.y += 0.01
+      plane.rotation.y += 0.01
+      plane.rotation.z += 0.01
       renderer.render(scene, camera)
     }
     animate()
+
+    //AUDIO ANALYSIS
+    let context = new (window.AudioContext || window.webkitAudioContext)()
+    let analyser = context.createAnalyser()
+    let soundDataArray
   }
 
   render() {
