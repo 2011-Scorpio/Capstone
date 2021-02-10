be187fa3d60b0f6323e2dbb2e9822b96e0240bdd
azrielgoldman <goldmanazriel@yahoo.com>
2021-02-10 12:09:28 -0500

    Added array with names and info to About component

--- client/components/About.js
+++ client/components/About.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
+const usersInfo = [
+  {name: 'Azriel Goldman', gitHub: '', linkedIn: ''},
+  {name: 'Gabriel Smith', gitHub: '', linkedIn: ''},
+  {name: 'Spencer Collins', gitHub: '', linkedIn: ''},
+  {name: 'Alfred Meng', gitHub: '', linkedIn: ''}
+]
+
 const About = () => {
   return <div />
 }
