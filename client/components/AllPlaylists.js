9da555acef9edcf914904570e4ef57c486ba22a4
Alfred <alfredfmeng@ALFRED-PC.localdomain>
2021-02-10 12:05:36 -0600

    added history.push

--- client/components/AllPlaylists.js
+++ client/components/AllPlaylists.js
@@ -22,6 +22,7 @@ class AllPlaylists extends React.Component {
 
   setCurrent(event) {
     this.props.setCurrentPlaylist(event.target.value)
+    this.props.history.push('/explore')
   }
 
   render() {
