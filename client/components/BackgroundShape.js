02261b19162ba53d55b610e0af1481ff6b112eff
Spencer Collins <smcollins36@gmail.com>
2021-02-10 15:05:20 -0600

    Added spinning logo to NavBar, needs formatting

--- client/components/BackgroundShape.js
+++ client/components/BackgroundShape.js
@@ -8,7 +8,7 @@ class BackgroundShape extends React.Component {
   componentDidMount() {
     let scene = new THREE.Scene()
     let camera = new THREE.PerspectiveCamera(
-      50,
+      75,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
