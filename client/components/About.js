a64b3f6dcaf4fb18aa81a0a40915bd7dc3ade9f3
Gabriel Smith <gabesmithp@gmail.com>
2021-02-11 11:31:57 -0500

    add: user img

--- client/components/About.js
+++ client/components/About.js
@@ -4,30 +4,34 @@ const usersInfo = [
   {
     name: 'Azriel Goldman',
     gitHub: 'https://github.com/azrielg20',
-    linkedIn: 'https://www.linkedin.com/in/azriel-goldman-67193b77/'
+    linkedIn: 'https://www.linkedin.com/in/azriel-goldman-67193b77/',
+    photo: '/images/user-img.jpg'
   },
   {
     name: 'Gabriel Smith',
     gitHub: 'https://github.com/gabesmithp',
-    linkedIn: 'https://www.linkedin.com/in/gabesmithp/'
+    linkedIn: 'https://www.linkedin.com/in/gabesmithp/',
+    photo: '/images/user-img.jpg'
   },
   {
     name: 'Spencer Collins',
     gitHub: 'https://github.com/spibcol',
-    linkedIn: 'https://www.linkedin.com/in/smcollins36/'
+    linkedIn: 'https://www.linkedin.com/in/smcollins36/',
+    photo: '/images/user-img.jpg'
   },
   {
     name: 'Alfred Meng',
     gitHub: 'https://github.com/alfredfmeng',
-    linkedIn: 'http://www.linkedin.com/in/alfredfmeng'
+    linkedIn: 'http://www.linkedin.com/in/alfredfmeng',
+    photo: '/images/user-img.jpg'
   }
 ]
 
 const About = () => {
   return (
-    <div>
+    <div className="about-container">
       {usersInfo.map(user => (
-        <div key={user.id}>
+        <div key={user.id} className="team-member">
           <div>Name: {user.name}</div>
           <a href={user.gitHub} target="_blank" rel="noreferrer">
             Github Profile
@@ -35,6 +39,7 @@ const About = () => {
           <a href={user.linkedIn} target="_blank" rel="noreferrer">
             LinkedIn Profile
           </a>
+          <img src={user.photo} alt="" />
         </div>
       ))}
     </div>
