e890b3dabac57c0bfa2553def2db7f1528957aba
Spencer Collins <smcollins36@gmail.com>
2021-02-12 15:33:42 -0600

    cleaned up unused files

--- server/auth/spotify.js
+++ server/auth/spotify.js
@@ -42,7 +42,7 @@ router.get(
 router.get(
   '/callback',
   passport.authenticate('spotify', {
-    successRedirect: '/explore',
+    successRedirect: '/',
     failureRedirect: '/login'
   })
 )
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,13 +1,10 @@
 const User = require('./user')
-const Playlist = require('./playlist')
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
  *
  *    BlogPost.belongsTo(User)
  */
-User.hasMany(Playlist)
-Playlist.belongsTo(User)
 /**
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
@@ -15,6 +12,5 @@ Playlist.belongsTo(User)
  * instead of: const User = require('../db/models/user')
  */
 module.exports = {
-  User,
-  Playlist
+  User
 }
