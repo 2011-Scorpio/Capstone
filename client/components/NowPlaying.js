b7dcc5132bc6bad05c9a6ee0ee46a0290366397a
Gabriel Smith <gabesmithp@gmail.com>
2021-02-14 18:49:09 -0500

    add: singleplaylist button in player view

--- client/components/NowPlaying.js
+++ client/components/NowPlaying.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {useHistory} from 'react-router-dom'
 
 const NowPlaying = props => {
+  const history = useHistory()
+
+  const goToSinglePlaylist = () => {
+    history.push('/singleplaylist')
+  }
   return (
     <>
       {props.currentPlaylistName ? (
         <div className="now-playing">
-          You're adding to: <strong>{props.currentPlaylistName.name}</strong>
+          You're adding to:{' '}
+          <button
+            type="button"
+            alt={`Go to ${props.currentPlaylistName.name}`}
+            onClick={goToSinglePlaylist}
+          >
+            {props.currentPlaylistName.name}
+          </button>
         </div>
       ) : (
         ''
