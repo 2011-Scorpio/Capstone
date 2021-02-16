052688ae6ed4c9fdd525651080bea61ead88a437
Gabriel Smith <gabesmithp@gmail.com>
2021-02-16 00:02:47 -0500

    add: spotify logo animation

--- client/components/Login.js
+++ client/components/Login.js
@@ -3,12 +3,10 @@ import React from 'react'
 const Login = () => {
   return (
     <div className="login-form">
-      <div id="spotify-logo-container">
-        <img className="spotify-logo" src="/images/spotify.png" alt="" />
-      </div>
       <a href="/auth/spotify" className="button-two">
         Log in to Spotify
       </a>
+      <img id="spotify-logo" src="/images/spotify.png" alt="" />
     </div>
   )
 }
